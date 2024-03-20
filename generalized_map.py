9ffec63d5f9d19550dd75ae1bb4a14c56e000910
Max von Hippel <maxvonhippel1996@gmail.com>
2024-03-20 12:00:58 -0700

    improved generalized script

--- generalized_map.py
+++ generalized_map.py
@@ -11,8 +11,27 @@ m=Basemap(llcrnrlon=-172, llcrnrlat=62.7, # corner 1
 
 # Continent in grey (change colors using hex codes if you want them to differ)
 m.fillcontinents(color='#808080', lake_color='#d3d3d3')
+# m.etopo(scale=3) # If you want color / topography, try this and comment out above line. 
+# In that case you may need to change the scale to increase resolution.  Just screw around
+# with it and see what works.
 m.drawcountries()
 m.drawrivers()
+
+# Add Latitude and Longitude Lines
+lat_start = 62.7 
+lat_end = 64 
+lat_spacing = 0.2
+
+lon_start = -172
+lon_end = -167.75
+lon_spacing = 1
+
+parallels = np.arange(lat_start, lat_end, lat_spacing) # Define the latitude range and interval
+meridians = np.arange(lon_start, lon_end, lon_spacing) # Define the longitude range and interval
+m.drawparallels(parallels, labels=[1,0,0,0], fontsize=10, linewidth=0.1) # Draw parallels (latitude lines). The labels list controls where the labels will be drawn
+m.drawmeridians(meridians, labels=[0,0,0,1], fontsize=10, linewidth=0.1) # Draw meridians (longitude lines). The labels list controls where the labels will be drawn
+
+
 
 # Show the coast lines in black
 m.drawcoastlines(color='black', linewidth=1)
